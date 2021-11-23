50d690741b6869f4feae87022bc14198490d4897
djohn232 <82186899+djohn232@users.noreply.github.com>
2021-11-22 19:35:17 -0600

    creating classes

---
+++ src/ClothingDriver.java
@@ -0,0 +1,33 @@
+/**************************************************************
+ * Name        : Project Name   
+* Author      : Delanie Johnson
+* Created     : 11/01/2021
+* Course      : CIS 152 Data Structures
+* Version     : 1.0
+* OS          : Windows 10
+* Copyright   : This is my own original work based on
+*               specifications issued by our instructor
+* Description : This program will suggest outfit selections based on the temperature 
+*               Input:  user will enter range of temps
+*               Output: shows temp and clothing suggestion based on user response
+* Academic Honesty: I attest that this is my original work.
+* I have not used unauthorized source code, either modified or 
+* unmodified. I have not given other fellow student(s) access to
+* my program.         
+***************************************************************/
+
+//Temperature Ranges:  
+//90 - 100+ - HOT
+//70 - 89 - WARM
+//50 - 69 - COOL
+//33 - 49 - COLD
+//32 - 0 (and less) - FREEZING
+
+public class ClothingDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Scorching.java
@@ -0,0 +1,10 @@
+//Temperature Ranges:  
+//90 - 100+ - HOT
+//70 - 89 - WARM
+//50 - 69 - COOL
+//33 - 49 - COLD
+//32 - 0 (and less) - FREEZING
+
+public class Scorching {
+
+}
